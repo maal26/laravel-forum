7adc8db3fd2efdf115f1627fd1e88c6e463b22fb
Mathews Lima <mathews.limaa@gmail.com>
2019-04-13 16:59:32 -0300

    Episode 19

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,18 @@
 <div class="card mb-3">
-    <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a>
-        said {{ $reply->created_at->diffForHumans() }}...
+    <div class="card-header d-flex align-items-center justify-content-between">
+        <div>
+            <a href="#">{{ $reply->owner->name }}</a>
+            said {{ $reply->created_at->diffForHumans() }}...
+        </div>
+        <div>
+            <form action="/replies/{{ $reply->id }}/favorites" method="post">
+                @csrf
+
+                <button class="btn btn-sm btn-outline-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites()->count() }} {{ \Str::plural('Favorite', $reply->favorites()->count()) }}
+                </button>
+            </form>
+        </div>
     </div>
     <div class="card-body">{{ $reply->body }}</div>
 </div>
